f0419b9433ff02bcb8e867d60c23cbe5ec8c15cb
Davron Gaziev <davron1104@yandex.com>
2023-01-01 11:47:24 +0500

    second

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 'use strict';
-alert('Hello world!!!');
+
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+document.addEventListener("DOMContentLoaded", () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('Start');
+        console.log(e.touches);
+    });
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchmove', (e) => {
+//         e.preventDefault();
+//         console.log('Move');
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchend', (e) => {
+//         e.preventDefault();
+//         console.log('End');
+//     });
+// });
+
+// touches
+// targettouches
+// changedTouches
